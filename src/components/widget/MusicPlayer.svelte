6ac019c03dc1977b47dd8d5a9ebcbdc533d7e0f8
hotokokoa233 <hotokokoa233@outlook.com>
2026-06-07 01:05:14 +0800

    2

--- src/components/widget/MusicPlayer.svelte
+++ src/components/widget/MusicPlayer.svelte
@@ -79,18 +79,18 @@ let volumeBar: HTMLElement;
 const localPlaylist = [
 	{
 		id: 1,
-		title: "ponponpon",
-		artist: "未知",
+		title: "ひとり上手",
+		artist: "Kaya",
 		cover: "assets/music/cover/hitori.jpg",
-		url: "assets/music/url/ponponpon.mp3",
+		url: "assets/music/url/hitori.mp3",
 		duration: 240,
 	},
 	{
 		id: 2,
-		title: "宝贝，不知道为什么",
+		title: "眩耀夜行",
 		artist: "スリーズブーケ",
 		cover: "assets/music/cover/xryx.jpg",
-		url: "assets/music/url/宝贝，不知道为什么.mp3",
+		url: "assets/music/url/xryx.mp3",
 		duration: 180,
 	},
 	{
