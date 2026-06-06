a01a7c07539f40bfc270970385e8e9fe4fa77787
hotokokoa233 <hotokokoa233@outlook.com>
2026-06-06 23:44:04 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -366,7 +366,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/head.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "绘梨花",
 	bio: "时光流转，愿有一天你能和你最重要的人重逢",
 	typewriter: {
