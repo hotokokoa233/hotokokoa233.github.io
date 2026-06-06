345f6187455712421e67ddd37537d10c7bd86f77
hotokokoa233 <hotokokoa233@outlook.com>
2026-06-06 23:16:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -24,8 +24,8 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "遇缘",
 	subtitle: "遇缘的博客",
-	siteURL: "https://mikufans.yuzhiyuan.top/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-11-04", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteURL: "https://hotokokoa233.github.io/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026.06.06", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
