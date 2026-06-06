264735fc44af877842f959d12ceb8d4b2d09a8a0
hotokokoa233 <hotokokoa233@outlook.com>
2026-06-06 23:54:46 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -22,10 +22,10 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: " 绘梨花",
-	subtitle: " 绘梨花的博客",
-	siteURL: "https://mikufans.yuzhiyuan.top/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-11-04", // 站点开始运行日期，用于站点统计组件计算运行天数
+	title: " 绘梨花 ",
+	subtitle: " 绘梨花的博客 ",
+	siteURL: "https://hotokokoa233.github.io/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2026-06-06", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
